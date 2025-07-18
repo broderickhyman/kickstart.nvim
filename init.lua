330b20cb79a0368a950e652b15c3519daeb0f4d4
Brodie Hyman <brodie.hyman@gentex.com>
2025-07-18 15:40:35 -0400

    Trying to auto start Roslyn

--- init.lua
+++ init.lua
@@ -288,6 +288,34 @@ require('lazy').setup({
     ---@type RoslynNvimConfig
     opts = {
       filewatching = 'roslyn',
+      choose_target = function(target)
+        return vim.iter(target):find(function(item)
+          if string.match(item, 'Lumen.sln') then
+            return item
+          end
+        end)
+      end,
+      config = {
+        settings = {
+          ['csharp|inlay_hints'] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ['csharp|code_lens'] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      },
     },
   },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
