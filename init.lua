fa03929695e1ca027db4a22641fd0921f382e590
Broderick Hyman <broderickhyman@gmail.com>
2025-09-06 23:49:58 -0400

    Use regular harpoon menu

--- init.lua
+++ init.lua
@@ -292,36 +292,44 @@ require('lazy').setup({
       harpoon:setup()
 
       -- basic telescope configuration
-      local conf = require('telescope.config').values
-      local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-          table.insert(file_paths, item.value)
-        end
+      -- local conf = require('telescope.config').values
+      -- local function toggle_telescope(harpoon_files)
+      --   local file_paths = {}
+      --   for _, item in ipairs(harpoon_files.items) do
+      --     table.insert(file_paths, item.value)
+      --   end
+      --
+      --   require('telescope.pickers')
+      --     .new({}, {
+      --       prompt_title = 'Harpoon',
+      --       finder = require('telescope.finders').new_table {
+      --         results = file_paths,
+      --       },
+      --       previewer = conf.file_previewer {},
+      --       sorter = conf.generic_sorter {},
+      --     })
+      --     :find()
+      -- end
 
-        require('telescope.pickers')
-          .new({}, {
-            prompt_title = 'Harpoon',
-            finder = require('telescope.finders').new_table {
-              results = file_paths,
-            },
-            previewer = conf.file_previewer {},
-            sorter = conf.generic_sorter {},
-          })
-          :find()
-      end
+      vim.keymap.set('n', '<leader>A', function()
+        harpoon:list():prepend()
+      end, { desc = 'Prepend to harpoon' })
 
       vim.keymap.set('n', '<leader>a', function()
         harpoon:list():add()
       end, { desc = 'Add to harpoon' })
 
-      vim.keymap.set('n', '<leader>d', function()
-        harpoon:list():remove()
-      end, { desc = 'Remove from harpoon' })
+      -- vim.keymap.set('n', '<leader>d', function()
+      --   harpoon:list():remove()
+      -- end, { desc = 'Remove from harpoon' })
+
+      -- vim.keymap.set('n', '<leader>e', function()
+      --   toggle_telescope(harpoon:list())
+      -- end, { desc = 'Open harpoon window' })
 
       vim.keymap.set('n', '<leader>e', function()
-        toggle_telescope(harpoon:list())
-      end, { desc = 'Open harpoon window' })
+        harpoon.ui:toggle_quick_menu(harpoon:list())
+      end)
 
       vim.keymap.set('n', '<C-j>', function()
         harpoon:list():select(1)
@@ -334,6 +342,19 @@ require('lazy').setup({
       end)
       vim.keymap.set('n', '<C-;>', function()
         harpoon:list():select(4)
+      end)
+
+      vim.keymap.set('n', '<leader><C-j>', function()
+        harpoon:list():replace_at(1)
+      end)
+      vim.keymap.set('n', '<leader><C-k>', function()
+        harpoon:list():replace_at(2)
+      end)
+      vim.keymap.set('n', '<leader><C-l>', function()
+        harpoon:list():replace_at(3)
+      end)
+      vim.keymap.set('n', '<leader><C-;>', function()
+        harpoon:list():replace_at(4)
       end)
 
       -- Toggle previous & next buffers stored within Harpoon list
