e7e2bcb0c032f0f1e183f8375848144a379905db
Broderick Hyman <broderickhyman@gmail.com>
2025-09-09 12:46:20 -0400

    Remove extra stuff

--- init.lua
+++ init.lua
@@ -881,14 +881,6 @@ require('lazy').setup({
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
