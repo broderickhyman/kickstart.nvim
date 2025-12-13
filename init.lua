8cf6eb462e07d2ea285ad8f7f7ad279afb85344f
Broderick Hyman <broderickhyman@gmail.com>
2025-12-13 15:28:17 -0500

    Update roslyn config

--- init.lua
+++ init.lua
@@ -390,73 +390,12 @@ require('lazy').setup({
   },
   {
     'seblyng/roslyn.nvim',
-    ft = { 'cs', 'razor' },
-    dependencies = {
-      {
-        -- By loading as a dependencies, we ensure that we are available to set
-        -- the handlers for Roslyn.
-        'tris203/rzls.nvim',
-        config = true,
-      },
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      -- your configuration comes here; leave empty for default settings
     },
-    config = function()
-      local mason_registry = require 'mason-registry'
-
-      local rzls_path = vim.fn.expand '$MASON/packages/rzls/libexec'
-      local cmd = {
-        'roslyn',
-        '--stdio',
-        '--logLevel=Information',
-        '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
-        '--razorSourceGenerator=' .. vim.fs.joinpath(rzls_path, 'Microsoft.CodeAnalysis.Razor.Compiler.dll'),
-        '--razorDesignTimePath=' .. vim.fs.joinpath(rzls_path, 'Targets', 'Microsoft.NET.Sdk.Razor.DesignTime.targets'),
-        '--extension',
-        vim.fs.joinpath(rzls_path, 'RazorExtension', 'Microsoft.VisualStudioCode.RazorExtension.dll'),
-      }
-
-      vim.lsp.config('roslyn', {
-        cmd = cmd,
-        handlers = require 'rzls.roslyn_handlers',
-        settings = {
-          ['csharp|inlay_hints'] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-
-            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-            csharp_enable_inlay_hints_for_types = true,
-            dotnet_enable_inlay_hints_for_indexer_parameters = true,
-            dotnet_enable_inlay_hints_for_literal_parameters = true,
-            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-            dotnet_enable_inlay_hints_for_other_parameters = true,
-            dotnet_enable_inlay_hints_for_parameters = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
-          },
-          ['csharp|code_lens'] = {
-            dotnet_enable_references_code_lens = true,
-          },
-        },
-      })
-      vim.lsp.enable 'roslyn'
-    end,
-    init = function()
-      -- We add the Razor file types before the plugin loads.
-      vim.filetype.add {
-        extension = {
-          razor = 'razor',
-          cshtml = 'razor',
-        },
-      }
-    end,
-    filewatching = 'roslyn',
-    choose_target = function(target)
-      return vim.iter(target):find(function(item)
-        if string.match(item, 'Lumen.sln') then
-          return item
-        end
-      end)
-    end,
+    ft = { 'cs', 'razor' },
   },
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
@@ -935,7 +874,6 @@ require('lazy').setup({
         'rust-analyzer',
         'codelldb',
         'html-lsp',
-        'rzls',
         'roslyn',
         'angular-language-server',
         'css-lsp',
